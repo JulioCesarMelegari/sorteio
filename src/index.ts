ff5f4d4b2b5ec7222d4a6ec25da2ddc606b0076c
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-11-19 18:09:25 -0300

    add logica selecionar o último sorteado

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import carregarPessoas from "./function/carregar-pessoas";
+import sortearAzarado from "./function/sortear-azarado";
 
 const pessoas = carregarPessoas("src/data/dados.csv");
+const azarado = sortearAzarado(pessoas);
+console.log(azarado.toString());
 
-pessoas.forEach((p) => console.log(p.toString()));
+//pessoas.forEach((p) => console.log(p.toString()));
+
